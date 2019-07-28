0e7fc4b9bbf6848f9ec460daeb2a9cf816f20e0d
Evan McCarter <evan.mccarter@gmail.com>
2019-07-28 02:48:40 -0400

    Explain purpose of file

--- basis.js
+++ basis.js
@@ -1,5 +1,7 @@
 // Sourced from https://www.youtube.com/watch?v=3VQ382QG-y4&list=PLpkHU923F2XFWv-XfVuvWuxq41h21nOPK&index=1 from https://github.com/glebec/lambda-talk
 
+// A basis that, when composed with each other, can generate all other functions
+
 // Kestrel
 a => b => a
 
