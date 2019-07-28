44fa2762fd01f6c404ebe6084077c478b162ced3
Evan McCarter <evan.mccarter@gmail.com>
2019-07-28 02:45:13 -0400

    Add

---
+++ basis.js
@@ -0,0 +1,7 @@
+// Sourced from https://www.youtube.com/watch?v=3VQ382QG-y4&list=PLpkHU923F2XFWv-XfVuvWuxq41h21nOPK&index=1 from https://github.com/glebec/lambda-talk
+
+// Kestrel
+a => b => a
+
+// Starling
+a => b => c => a (c) (b (c))
